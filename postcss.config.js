5d2ab127a53798f0348b3d6d5fbb21e5b9d5e6d8
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-06-01 01:26:23 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+@parcel/transformer-postcss: O pacote inclui transpilação CSS e fornecedor
+prefixo por padrão. A configuração do PostCSS postcss.config.js contém o seguinte
+plugins redundantes : autoprefixer. Removê-los pode melhorar o desempenho da compilação .
+  /opt/render/project/src/postcss.config.js:1:1
+  > 1 | module.exports = {
+  > | ^
+    2 | plugins: {
+    3 | tailwindcss: {},
+💡 Remova os plugins acima de postcss.config.js
+📝 Saiba mais: https://parceljs.org/languages/css/#default-plugins
